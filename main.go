000074631e71d6d0c475bea0ba3739e13dadb9b0
calmdaysamuel <calmdaysamuel@gmail.com>
2026-01-10 19:23:49 -0800

    Sample main

--- main.go
+++ main.go
@@ -1,48 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/calmdaysamuel/cheesecake/application"
-	"github.com/calmdaysamuel/cheesecake/mouseactions"
-	"github.com/calmdaysamuel/cheesecake/widget"
 	"github.com/calmdaysamuel/cheesecake/widgets/text"
 	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
-	_ = application.Start(&Model{})
-}
-
-var _ widget.StatefulWidget = &Model{}
-
-type Model struct {
-}
-type State struct {
-	ID        string
-	TextStyle lipgloss.Style
-}
-
-func (s *State) Identifier() string {
-	return s.ID
-}
-
-func (s *State) Init() {}
-
-func (s *State) Dispose() {}
-
-func (m *Model) Element() widget.State {
-	return &State{}
-}
-
-func (m *Model) Build(ctx context.Context, element widget.State) widget.Widget {
-	state := element.(*State)
-	return text.New("hello", text.WithTextStyle(state.TextStyle), text.WithMouseActions(mouseactions.Manager{
-		OnHoverStateChangeFunc: nil,
-		OnClickFunc: func() {
-			fmt.Println("hello clicked")
-			state.TextStyle = lipgloss.NewStyle().Background(lipgloss.Color("5"))
-		},
-	}))
+	_ = application.Start(&text.InteractiveModel{
+		Text:         "Hello Samuel",
+		DefaultStyle: lipgloss.NewStyle().Background(lipgloss.Color("5")),
+		HoverStyle:   lipgloss.NewStyle().Background(lipgloss.Color("6")),
+		ActiveStyle:  lipgloss.NewStyle().Background(lipgloss.Color("7")),
+	})
 }
